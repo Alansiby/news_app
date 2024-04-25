487306bc0ba9ff511acdde2667cfa3aae6edbca4
Alansiby <alansibyp2001@gmail.com>
2024-04-25 21:42:01 +0530

    Category

--- lib/controller/home_screen_controller.dart
+++ lib/controller/home_screen_controller.dart
@@ -5,15 +5,27 @@ import 'package:http/http.dart' as http;
 import 'package:news_app/model/news_api_res_model.dart';
 
 class HomeScreenController with ChangeNotifier {
-  NewsApiResModel? resModel;
+  static List<String> categoryList = [
+    "business",
+    "entertainment",
+    "general",
+    "health",
+    "science",
+    "sports",
+    "technology"
+  ];
 
+  NewsApiResModel? resByCategory;
+  int selectedCatagoryIndex = 0;
+  NewsApiResModel? resModel;
   bool isLoading = false;
 
-  Future getData() async {
+  //get data by category
+  Future getDataByCategory() async {
     isLoading = true;
     notifyListeners();
     Uri url = Uri.parse(
-        "https://newsapi.org/v2/everything?q=bitcoin&apiKey=bbfa9427d470424cba349097b404a7ba");
+        "https://newsapi.org/v2/top-headlines?country=in&category=${categoryList[selectedCatagoryIndex]}&apiKey=bbfa9427d470424cba349097b404a7ba");
 
     var res = await http.get(url);
 
@@ -26,4 +38,30 @@ class HomeScreenController with ChangeNotifier {
     isLoading = false;
     notifyListeners();
   }
+
+  //on category selection
+  onCategorySelection(int value) {
+    selectedCatagoryIndex = value;
+    notifyListeners();
+    getDataByCategory();
+  }
+
+// get data of everything
+  // Future getData() async {
+  //   isLoading = true;
+  //   notifyListeners();
+  //   Uri url = Uri.parse(
+  //       "https://newsapi.org/v2/everything?q=bitcoin&apiKey=bbfa9427d470424cba349097b404a7ba");
+
+  //   var res = await http.get(url);
+
+  //   if (res.statusCode == 200) {
+  //     var decodeData = jsonDecode(res.body);
+  //     resModel = NewsApiResModel.fromJson(decodeData);
+  //   } else {
+  //     print("failed");
+  //   }
+  //   isLoading = false;
+  //   notifyListeners();
+  // }
 }
--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, avoid_unnecessary_containers, unnecessary_string_interpolations
 
 import 'package:flutter/material.dart';
 import 'package:news_app/controller/home_screen_controller.dart';
@@ -14,32 +14,86 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
-      await Provider.of<HomeScreenController>(context, listen: false).getData();
-    });
+    WidgetsBinding.instance.addPostFrameCallback(
+      (timeStamp) async {
+        await context.read<HomeScreenController>().getDataByCategory();
+      },
+    );
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     final provider = context.watch<HomeScreenController>();
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.read<HomeScreenController>().getData();
-        },
+    return DefaultTabController(
+      length: HomeScreenController.categoryList.length,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            isScrollable: true,
+            onTap: (value) {
+              context.read<HomeScreenController>().onCategorySelection(value);
+            },
+            tabs: List.generate(
+              HomeScreenController.categoryList.length,
+              (index) => Tab(
+                child: Text(
+                    "${HomeScreenController.categoryList[index].toUpperCase()}"),
+              ),
+            ),
+          ),
+        ),
+        // floatingActionButton: FloatingActionButton(
+        //   onPressed: () {
+        //     context.read<HomeScreenController>().getData();
+        //   },
+        // ),
+        body: provider.isLoading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView.separated(
+                padding:
+                    EdgeInsets.only(bottom: 10, top: 10, right: 10, left: 10),
+                itemCount: provider.resModel?.articles?.length ?? 0,
+                itemBuilder: (context, index) {
+                  return
+                      // ListTile(
+                      //   title:
+                      //       Text(provider.resModel?.articles?[index].title ?? ""),
+                      // );
+                      Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color.fromARGB(255, 129, 116, 116),
+                    ),
+                    // color: Colors.grey.shade100,
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 150,
+                          child: Image.network(
+                              fit: BoxFit.cover,
+                              "${provider.resModel?.articles?[index].urlToImage}"),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                            "${provider.resModel?.articles?[index].title?.toUpperCase()}"),
+                        Text(
+                          "${provider.resModel?.articles?[index].description?.toUpperCase()}",
+                          style: TextStyle(fontSize: 10),
+                        )
+                      ],
+                    ),
+                  );
+                },
+                separatorBuilder: (context, index) => SizedBox(
+                      height: 10,
+                    )),
       ),
-      body: provider.isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: provider.resModel?.articles?.length ?? 0,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(provider.resModel?.articles?[index].title ?? ""),
-                );
-              }),
     );
   }
 }
