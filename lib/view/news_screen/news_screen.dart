fbc7a067c6d748088395f9a0e569c3eee133f64a
Alansiby <alansibyp2001@gmail.com>
2024-04-26 18:25:00 +0530

    Read News

--- lib/view/news_screen/news_screen.dart
+++ lib/view/news_screen/news_screen.dart
@@ -3,6 +3,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/model/news_api_res_model.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NewsScreen extends StatelessWidget {
   const NewsScreen({super.key, required this.article});
@@ -17,7 +18,7 @@ class NewsScreen extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
-            color: const Color.fromARGB(255, 129, 116, 116),
+            // color: const Color.fromARGB(255, 129, 116, 116),
           ),
           // color: Colors.grey.shade100,
           child: Column(
@@ -56,7 +57,11 @@ class NewsScreen extends StatelessWidget {
               SizedBox(
                 height: 10,
               ),
-              ElevatedButton(onPressed: () {}, child: Text("Read More"))
+              ElevatedButton(
+                  onPressed: () async {
+                    !await launchUrl(Uri.parse(article?.url ?? ""));
+                  },
+                  child: Text("Read More"))
             ],
           ),
         ));
